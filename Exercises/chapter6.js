0290bb5bb6cff92f6ece7739bb3dca3a86fa15f0
ShaneLucy <shanelucy01@protonmail.com>
2020-04-08 12:59:11 +0100

    Completed chapter 6 exercises

--- Exercises/chapter6.js
+++ Exercises/chapter6.js
@@ -47,13 +47,112 @@ a member of the group. Finally it should have a static method which takes an ite
 produced by iterating over it
 */
 
+class Group {
+  constructor() {
+    this.members = [];
+  }
+
+  add(value) {
+    if (!this.has(value)) {
+      this.members.push(value);
+    }
+  }
+
+  delete(value) {
+    this.members = this.members.filter(v => v !== value);
+  }
+
+  has(value) {
+    return this.members.includes(value);
+  }
+
+  static from(collection) {
+    let group = new Group;
+    for (let value of collection) {
+      group.add(value);
+    }
+    return group;
+  }
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+//true
+console.log(group.has(30));
+//false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+
+
 /*
 Exercise three
 Make the group class from the previous exercise iterable
 */
+class Group2 {
+  constructor() {
+    this.members = [];
+  }
+
+  add(value) {
+    if (!this.has(value)) {
+      this.members.push(value);
+    }
+  }
+
+  delete(value) {
+    this.members = this.members.filter(v => v !== value);
+  }
+
+  has(value) {
+    return this.members.includes(value);
+  }
+
+  static from(collection) {
+    let group2 = new Group2;
+    for (let value of collection) {
+      group2.add(value);
+    }
+    return group2;
+  }
+
+  [Symbol.iterator]() {
+    return new GroupIterator(this);
+  }
+}
+
+class GroupIterator {
+  constructor(group2) {
+    this.group = group2;
+    this.position = 0;
+  }
+
+  next() {
+    if (this.position >= this.group.members.length) {
+      return {done: true};
+    } else {
+      let result = {value: this.group.members[this.position],
+                    done: false};
+      this.position++;
+      return result;
+    }
+  }
+}
+
+for (let value of Group2.from(["a", "b", "c"])) {
+  console.log(value);
+}
+//a
+//b
+//c
 
 /*
 Exercise four
 Can you think of a way to call hasOwnProperty on an object that has
 its own property by that name?
 */
+
+let map = {one: true, two: true, hasOwnProperty: true};
+
+console.log(Object.prototype.hasOwnProperty.call(map, "one"));
+//true
